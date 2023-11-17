8b29801156f6f80800dca3820a456a40fc3ac52a
root <root@chenyltestllama2.hluzrx0tokyu3cccqgfakuh4qe.ix.internal.cloudapp.net>
2023-11-17 10:57:31 +0000

    uodate

--- src/signalr/Program.cs
+++ src/signalr/Program.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using LLama.Native;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
@@ -10,6 +11,7 @@ namespace Microsoft.Azure.SignalR.Samples.ChatRoom
     {
         public static void Main(string[] args)
         {
+            NativeLibraryConfig.Default.WithLogs();
             CreateWebHostBuilder(args).Build().Run();
         }
 
